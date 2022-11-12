a769a474fd26ae52f1146a8a2f09973c3b7a1b12
Silvinarodriguez1986 <114448367+Silvinarodriguez1986@users.noreply.github.com>
2022-11-12 18:50:05 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function derechos()
+{alert("Todos los derechos reservados ® Rodriguez Silvina 2022")
+}
+
+function modonoche() {
+document.body.style.backgroundColor= "black";
+document.getElementById("header").style.backgroundColor = "black";
+document.getElementById("header").style.color = "white";
+document.getElementById("nav").style.backgroundColor = "black";
+document.getElementById("nav").style.color = "white";
+document.getElementById("aside").style.backgroundColor = "black";
+document.getElementById("aside").style.color = "white";
+document.getElementById("section").style.backgroundColor = "black";
+document.getElementById("section").style.color = "white";
+document.getElementById("footer").style.backgroundColor = "black";
+document.getElementById("footer").style.color = "white";
+}
